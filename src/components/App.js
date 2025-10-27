21560f36daadb175bbfccc10e8db9215cbe287a3
simonkaruga <simonkaruganjoroge@gmail.com>
2025-10-27 15:12:25 +0300

    Add origin

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,37 @@
-import React, { useState } from "react";
+import React from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
-function App() {
-  const [items, setItems] = useState(itemData);
-  const [isDarkMode, setIsDarkMode] = useState(false);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: itemData,
+      isDarkMode: false,
+    };
 
-  function handleDarkModeClick() {
-    setIsDarkMode((isDarkMode) => !isDarkMode);
+    this.handleDarkModeClick = this.handleDarkModeClick.bind(this);
   }
 
-  return (
-    <div className={"App " + (isDarkMode ? "dark" : "light")}>
-      <header>
-        <h2>Shopster</h2>
-        <button onClick={handleDarkModeClick}>
-          {isDarkMode ? "Dark" : "Light"} Mode
-        </button>
-      </header>
-      <ShoppingList items={items} />
-    </div>
-  );
+  handleDarkModeClick() {
+    this.setState((state) => ({ isDarkMode: !state.isDarkMode }));
+  }
+
+  render() {
+    const { items, isDarkMode } = this.state;
+
+    return (
+      <div className={"App " + (isDarkMode ? "dark" : "light")}>
+        <header>
+          <h2>Shopster</h2>
+          <button onClick={this.handleDarkModeClick}>
+            {isDarkMode ? "Dark" : "Light"} Mode
+          </button>
+        </header>
+        <ShoppingList items={items} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,24 +1,36 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Item({ name, category }) {
-  const [isInCart, setIsInCart] = useState(false);
+class Item extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isInCart: false,
+    };
 
-  function handleAddToCartClick() {
-    setIsInCart((isInCart) => !isInCart);
+    this.handleAddToCartClick = this.handleAddToCartClick.bind(this);
   }
 
-  return (
-    <li className={isInCart ? "in-cart" : ""}>
-      <span>{name}</span>
-      <span className="category">{category}</span>
-      <button
-        className={isInCart ? "remove" : "add"}
-        onClick={handleAddToCartClick}
-      >
-        {isInCart ? "Remove From" : "Add to"} Cart
-      </button>
-    </li>
-  );
+  handleAddToCartClick() {
+    this.setState((state) => ({ isInCart: !state.isInCart }));
+  }
+
+  render() {
+    const { name, category } = this.props;
+    const { isInCart } = this.state;
+
+    return (
+      <li className={isInCart ? "in-cart" : ""}>
+        <span>{name}</span>
+        <span className="category">{category}</span>
+        <button
+          className={isInCart ? "remove" : "add"}
+          onClick={this.handleAddToCartClick}
+        >
+          {isInCart ? "Remove From" : "Add to"} Cart
+        </button>
+      </li>
+    );
+  }
 }
 
 export default Item;
